58733bb5a02e5dbae08a0bd9869c7e5a0f29de80
Lauramendzvie <laura.mvs0608@gmail.com>
2025-08-07 16:52:12 -0300

    Tabela Autor

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+    nacionality = models.CharField(max_length=30, null=True, blank=True)
+    biography = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+    
+        
